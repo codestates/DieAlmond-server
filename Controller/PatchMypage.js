b38ad62b92dce3d4a0c3528a3ce966de57ea6a5b
kilo718 <kilo718@naver.com>
2021-07-01 14:20:23 +0900

    :memo: :hammer: feat/mypage-validCheck

--- Controller/PatchMypage.js
+++ Controller/PatchMypage.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 const User = require('../Database/model/User')
-
+//ya29.a0ARrdaM-9LJzD0veBAijIOvXg9uzhs75i_LMmpkVwpRTIqUU_9AdzWrdFS3CpT1BZWIfzhjPUgZAt011TwoHu9usYQCvRw9OSr6klA-iO32T507VOgMvKDuHYajY0ZRGyOCQpVpQ9z8n-atjdts3mpH_tvrcOvg
 module.exports = async (req, res) => {
   if (req.headers.authorization) {
     let access_token = req.headers.authorization
-    if (req.headers.sns === 'kakao') {
+    if (req.headers.sns === 'kakao') {  ////////////////////////////////////////////////////////////////kakao
       await axios.get('https://kapi.kakao.com/v2/user/me', {  //카카오에 엑세스 토큰을 보내서 유저정보 요청
         headers: {
           'authorization': access_token
@@ -15,7 +15,10 @@ module.exports = async (req, res) => {
         if (!userInfo) {  // 회원 가입되어있는지 확인함.
           res.status(401).send('invalid token')
         } else {
-          User.updateMany({ 'email': userInfo.email },  // save()는 모든 필드 덮어쓰기라서 기존에 있던 내용 사라짐
+          let valid = await User.findOne({'nickname': req.body.nickname})
+
+          if(!valid){
+            User.updateMany({ 'email': userInfo.email },  // save()는 모든 필드 덮어쓰기라서 기존에 있던 내용 사라짐
             {
               $set: {
                 'sleep': req.body.sleep,
@@ -25,13 +28,16 @@ module.exports = async (req, res) => {
                 'gender': req.body.gender,
                 'nickname': req.body.nickname
               }
-            }).then('업데이트 성공').catch(err => console.log(err))
-          //DB 쓰기 성공 or 실패
-
-          res.status(200).send('수정완료')
+            }).then(res.status(200).send('수정완료')).catch(err => console.log("Controller/SnsLogin/PatchMypage :28 DB update ERROR",err))  //DB 쓰기 성공 or 실패
+          }else{                   
+            res.status(409).send({'code':-409,'msg':'Duplicate Nickname'})
+          }
         }
-      }).catch(err => console.log(err, "axios ERROR"))  //axios error
-    } else if (req.headers.sns === 'google') {
+      }).catch((err) => {
+        console.log("Controller/SnsLogin/PatchMypage :34 axios ERROR",err)
+        res.status(401).send('Expired Token')
+      })  //axios error
+    } else if (req.headers.sns === 'google') {   ///////////////////////////////////////////////////// google
       await axios('https://www.googleapis.com/oauth2/v3/userinfo',
         {
           method: 'GET',
@@ -44,7 +50,12 @@ module.exports = async (req, res) => {
           if (!userInfo) {
             res.status(401).send('invalid token')
           } else {
-            User.updateOne({ 'email': userInfo.email },  // 파라미터로 받은 데이터 db에서 수정
+            let valid = await User.findOne({'nickname': req.body.nickname})  //닉네임 중복체크
+            if(valid.email = userInfo.email){
+              valid = null
+            }
+            if(!valid){
+              User.updateOne({ 'email': userInfo.email },  // 파라미터로 받은 데이터 db에서 수정
               {
                 $set: {
                   'sleep': req.body.sleep,
@@ -54,13 +65,27 @@ module.exports = async (req, res) => {
                   'gender': req.body.gender,
                   'nickname': req.body.nickname
                 }
-              }).then('업데이트 성공').catch(err => console.log(err))
-
-            res.status(200).send('수정완료')
+              }).then(res.status(200).send('수정완료')).catch((err) => 
+              {
+                console.log('Controller/SnsLogin/PatchMypage : 68 /DB update ERROR',err)
+                res.send({'code':404,'msg':'Please try again in a few minutes.'})
+              })    // db 업데이트 에러
+            }else{
+              res.status(409).send({'code':-409,'msg':'Duplicate Nickname'})
+            }
+          }
+        }).catch((err) => {
+          console.log('Controller/SnsLogin/PatchMypage :77 axios ERROR ',err)
+          if(err.response.status === 401){
+            res.status(401).send({'code':-401,'msg':err.response.statusText})
+          }else if(err.response.status === 403){
+            res.status(403).send({'code':-401,'msg':err.response.statusText})
           }
         })
+    }else{
+      res.status(401).send({'code':401,'msg':'Unknown sns Token'})  // 
     }
   } else {   // not found token    
-    res.status(400).send('not found token')  //
+    res.status(400).send({'code':400,'msg':'not found token'})  //
   }
 }
