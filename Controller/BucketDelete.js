98bacdaefe5b2929b112a5f32b34941b9d4091f3
kilo718 <kilo718@naver.com>
2021-07-02 19:50:12 +0900

    :memo: :hammer: feat/BucketDelete-NoticeBoardDelete edit:Contorller/BucketDelete

--- Controller/BucketDelete.js
+++ Controller/BucketDelete.js
@@ -1,32 +1,27 @@
 const axios = require('axios')
+const BucketList = require('../Database/model/BucketList')
 const User = require("../Database/model/User")
+const userDataRemover = require('../middleware/UserDataRemover')
+
 
 module.exports = async(req, res) => {
   if (req.headers.authorization) {
     let access_token = req.headers.authorization
-    if(req.headers.sns === 'kakao'){
+    if(req.headers.sns === 'kakao'){ ////////////////////////////kakao
       await axios.get('https://kapi.kakao.com/v2/user/me',{
         headers:{
           'authorization':access_token
         }
       }).then(async (kakaoData)=>{
         let userInfo = await User.findOne({'email':kakaoData.data.kakao_account.email})
+        
 
         if(!userInfo) {  
           res.status(401).send('invalid token')
-        }else{
-          let allList = userInfo.list
-
-          for(let i in allList){   // 받아온 id 값을 기준으로 list 배열에서 삭제
-            if(allList[i].id === req.body.id){
-              delete allList[i]
-              break;  // id값은 중복될 수 없음 불필요한 연산 제거
-            }
-          }
-
-          allList = allList.filter((item)=>{
-            return 'empty' !== item || 'undefined' !== item
-          })
+        }else{  ////////////////////////////////////////  delete user.list    target
+          let allList = userDataRemover(userInfo.list, req.body.id) 
+          BucketList.deleteOne({'id':req.body.id}).catch('Controller/BucketDelete :23 db ERROR',err)
+          //공유된 버킷리스트 데이터 삭제.
 
           User.updateOne({'email':userInfo.email},{
             $set:{'list':allList}
@@ -34,7 +29,7 @@ module.exports = async(req, res) => {
           .catch(err => console.log('Controller/BucketDelte DB ERROR', err))
         }
       })
-    }else if(req.headers.sns === 'google'){
+    }else if(req.headers.sns === 'google'){  ////////////////////////google
       await axios('https://www.googleapis.com/oauth2/v3/userinfo',
       {
         method:'GET',
@@ -46,13 +41,10 @@ module.exports = async(req, res) => {
 
         if(!userInfo){
           res.status(401).send('invalid token')
-        }else{
-          let allList = userInfo.list
-          delete allList[req.body.bucketid]
-
-          allList = allList.filter((item)=>{
-            return 'empty' !== item || 'undefined' !== item
-          })
+        }else{  ////////////////////////////////////////  delete user.list    target
+          let allList = userDataRemover(userInfo.list, req.body.id)
+          BucketList.deleteOne({'id':req.body.id}).catch('Controller/BucketDelete :23 db ERROR',err)
+          //공유된 버킷리스트 데이터 삭제.
 
           User.updateOne({'email':userInfo.email},{
             $set:{'list':allList}
