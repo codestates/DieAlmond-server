30891ccdc1d553b08b09156b5b1c244f6262b6a2
kilo718 <kilo718@naver.com>
2021-07-02 20:53:05 +0900

    :memo: feat/WithDrawal edit:Controller/WithDrawal

---
+++ middleware/AllRemover.js
@@ -0,0 +1,9 @@
+const BucketList = require('../Database/model/BucketList')
+
+
+module.exports = (list) => {  // 해당 유저의 모든 게시물 삭제
+  for (let i in list) {
+    BucketList.deleteOne({ 'id': list[i].id }).catch(err => console.log('AllRemover error', err))
+  }
+  return 1;
+}
