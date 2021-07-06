a08a1f8d44085e4e4409621ef6083cf3d230566d
kilo718 <kilo718@naver.com>
2021-07-06 20:15:26 +0900

    :memo: feat/SignOut Controller/SignOut

--- Controller/SignOut.js
+++ Controller/SignOut.js
@@ -1,3 +1,14 @@
-module.exports = (req,res)=>{
-    res.send('get Signout')
+const tokenVerify = require('../middleware/TokenVerify')
+const User = require('../Database/Model/BucketList')
+
+module.exports = async (req, res) => {
+  let verifyData = tokenVerify(req, res)
+
+  let userinfo = await User.findOne({ 'email': verifyData.email })
+
+  if (userinfo) {
+    res.status(200).send('로그아웃 완료')
+  } else {
+    res.status(400).send('잘못된 접근')
+  }
 }
