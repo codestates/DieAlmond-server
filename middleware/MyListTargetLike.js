86d5886f8bceb64fecc35432a5b09c06f52ace1f
kilo718 <kilo718@naver.com>
2021-07-07 15:36:02 +0900

    test

--- middleware/MyListTargetLike.js
+++ middleware/MyListTargetLike.js
@@ -1,17 +1,21 @@
 const User = require('../Database/Model/User')
 const remover = require('../middleware/UserDataRemover')
+//targetUser 는 좋아요 누른 게시물의 주인
+// userinfo 는 좋아요를 누른 사람
+
+
 
 module.exports = async (req,userinfo,targetUser,option) => {
   let like
   
   for(let i in userinfo.list){
-    if(targetUser.list[i].bucketid === req.body.bucketid){
-      if(!option){
-        like = [...targetUser.list[i].like,{'id':userinfo.nickname}]
-      }else{
+    if(targetUser.list[i].bucketid === req.body.bucketid){ // 게시물 주인의 list에서 요청으로 받은 id와 같은게 있는지 찾음
+      if(!option){  //찾았다면 option 유무에 따라 지우거나 추가
+        like = [...targetUser.list[i].like,{'id':userinfo.nickname}] 
+      }else{ // 옵션에 무언가 들어있다면 remove명령
         like = remover(targetUser.list[i].like,userinfo.nickname)
       }
-      // targetUser는 해당 게시물의 주인, userinfo는 해당 게시물을 누른 타인 게시물주인의 targetlist에 좋아요 누른 타인의 닉네임을 등록 
+      
       targetUser.list[i].like = like // 수정된 like로 변경
       await User.updateOne({'email':targetUser.email},
       {
@@ -20,7 +24,6 @@ module.exports = async (req,userinfo,targetUser,option) => {
       break;
     }
   }
-
 }
 
 // 우선 target 유저 userinfo 에서 list 배열을 순회한다.
